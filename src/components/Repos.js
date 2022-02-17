7700c8bd3165809a50e6b833cbbfec420d485e3f
Thanh Nguyen <jamesnguyen1st@gmail.com>
2022-02-17 22:45:12 +0700

    add doughnut2d chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -35,6 +35,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={languages} />
+        <div></div>
       </Wrapper>
     </section>
   );
